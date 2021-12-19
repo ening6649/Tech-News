1a8aa5f5da3ffd50aaed6eca3dd5e0e1fb32f5d4
ening <ening6649@yahoo.com>
2021-12-19 13:10:02 -0500

    edited

---
+++ notes.js
@@ -0,0 +1,231 @@
+// object-relational mapping (Links to an external site.), or ORM
+// always run npm init or npm init -y first 
+// "start": "node server.js" in package.json
+// npm install express sequelize mysql2     to instill multiple packages at once
+
+// first step in using sequelize 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    // TABLE COLUMN DEFINITIONS GO HERE
+    // this is the first object passed, defines 
+    // the columns and data types for those columns
+  },
+  {
+    // second object passed 
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
+
+// continues from the above 
+User.init(
+    {
+      // define an id column
+      id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        // instruct that this is the Primary Key
+        primaryKey: true,
+        // turn on auto increment
+        autoIncrement: true
+      },
+      // define a username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      // define an email column
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // there cannot be any duplicate email values in this table
+        unique: true,
+        // if allowNull is set to false, we can run our data through validators before creating the table data
+        validate: {
+          isEmail: true
+        }
+      },
+      // define a password column
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          // this means the password must be at least four characters long
+          len: [4]
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+
+//   
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
+router.post('/', (req, res) => {});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
+
+// setting up the first api from the above
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    // findall is a model class' method 
+    // query all fo the users from the user table
+    User.findAll()
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// continues from the above
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    // sequelize method findone instead of find all
+    User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    // pass in req.body to provide the new data we want to use in the update
+    User.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// index routes 
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+// update get /api/users route to exclude password
+User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
